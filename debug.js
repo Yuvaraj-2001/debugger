41b21cdc1ec53fe759761220ee2e70772a16188d
Yuvaraj-2001 <62696754+Yuvaraj-2001@users.noreply.github.com>
2021-07-13 11:23:25 +0530

    Add files via upload

--- debug.js
+++ debug.js
@@ -23,3 +23,29 @@ function generateDebug(){
 
     document.getElementById('converted').value = result;
 }
+
+function generateDebugAngular(){
+    debugger
+    // var text = document.getElementById('convert').value;
+    var textArea = document.getElementById("convert");
+    var arrayFromTextArea = textArea.value.split(String.fromCharCode(10));
+    console.log(arrayFromTextArea);
+    document.getElementById('converted').value = arrayFromTextArea;
+    // var m = document.getElementById('converted').value;
+    var text = [];
+    for(let i = 0; i < arrayFromTextArea.length; i++){
+       if(arrayFromTextArea[i].indexOf("){") !== -1 || arrayFromTextArea[i].indexOf(") {") !== -1) {
+           debugger
+           text.push(arrayFromTextArea[i]);
+           text.push( "debugger")
+       }else{
+       text.push(arrayFromTextArea[i]);
+       }
+    }
+    let result = "";
+    for(let i = 0; i < text.length; i++){
+        result = result + text[i] + "\n";
+    }
+
+    document.getElementById('converted').value = result;
+}
